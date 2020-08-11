f0e8d8c0794bb98dd45754e5107a23ee6f6ebfc6
Matheus Cunha <matheuz.a@gmail.com>
2020-08-11 23:17:11 +0200

    This is not the first commit message you're looking for

---
+++ README.md
@@ -0,0 +1,90 @@
+<h1 align="center">AwesomeWM Media Player Widget</h1>
+
+Pure Lua* implementation of a Media Player Widget, supports any media player
+complaint with [MPRIS D-Bus interface](https://specifications.freedesktop.org/mpris-spec/latest/)
+specification.
+
+Some examples are: mpd, vlc, audacious, bmp, xmms2, spotify and [mpv with a
+plugin](https://github.com/hoyon/mpv-mpris)
+
+\* Pure Lua except for [Playerctl's][playerctl] sub-shell invocations.
+   TODO: Migrate to Lua D-Bus proxy
+
+![Screenshot](/screenshot.png?raw=true "Screenshot")
+
+## Installation
+
+The requirements for this widget are:
+ * [Lua D-Bus Proxy](https://github.com/stefano-m/lua-dbus_proxy) handles
+   connections with D-Bus interface to read data regarding current playing music
+   and artist to fill the widget. Lua D-Bus proxy is available [at Luarocks.](https://luarocks.org/modules/stefano-m/dbus_proxy)
+   Further installation info [available here](https://github.com/stefano-m/lua-dbus_proxy#installation)
+   
+ * [Playerctl][playerctl] interacts with the media player using mouse gestures
+   to play, pause and change to next and previous "content". Playerctl is a
+   Generic MPRIS D-Bus controller. Mouse scroll up and down changes the
+   "content". NOTE: "content" == music/video/media.
+
+Drop the plugin code into your AwesomeWM configuration folder. e.g.:
+
+```bash
+[[ -d ~/.config/awesome/plugins ]] || mkdir ~/.config/awesome/plugins
+cd ~/.config/awesome/plugins
+
+git clone https://github.com/macunha1/awesomewm-media-player-widget \
+    ~/.config/awesome/plugins/media-player
+```
+
+In case your AwesomeWM configuration is a git repository, you can add as a
+submodule with
+
+``` bash
+cd ~/.config/awesome
+
+git submodule add -b master \
+    -f --name media-player-widget \
+    https://github.com/macunha1/awesomewm-media-player-widget \
+    plugins/media-player
+
+git submodule sync --recursive .
+```
+
+### Usage
+
+Lastly, import the plugin into your `rc.lua`, configure according to your style
+and _Keep on Rockin'_:
+
+```lua
+-- load the widget code
+local media_player = require("plugins.media-player")
+
+-- be free to configure your media player widget:
+local spotify_widget = media_player({
+    icons  = {
+        play   = theme.play,
+        pause  = theme.pause
+    },
+    font         = theme.font,
+    name         = "spotify", -- target media player
+    refresh_rate = 0.3
+}).widget
+
+fake.wibar:set {
+    layout = wibox.layout.align.horizontal,
+    -- ...
+    {
+        -- ...
+        separator,
+        media_player_widget,
+        -- ...
+    }
+    -- ...
+}
+```
+
+By default the widget hides itself when no data regarding a media player is
+found through MPRIS D-Bus interface. In case you're planning to inspect many at
+the same time, it's possible to create many instances of the widget
+(`media_player`) and attach to your wibar.
+
+[playerctl]: https://github.com/altdesktop/playerctl
---
+++ init.lua
@@ -0,0 +1,149 @@
+local awful       = require("awful")
+local beautiful   = require("beautiful")
+local helpers     = require("lain.helpers")
+local proxy       = require("dbus_proxy")
+local wibox       = require("wibox")
+
+local MediaPlayer = {}
+
+function MediaPlayer:new(args)
+	return setmetatable({}, {__index = self}):init(args)
+end
+
+function MediaPlayer:init(args)
+	self.icons  = args.icons      or {
+		play    = args.play_icon  or beautiful.play,
+		pause   = args.pause_icon or beautiful.pause
+	}
+	self.font   = args.font       or beautiful.font
+	
+	self.widget = wibox.widget {
+		{
+			id = "icon",
+			widget = wibox.widget.imagebox,
+		},
+		{
+			id = 'current_song',
+			widget = wibox.widget.textbox,
+			font = self.font
+		},
+		layout = wibox.layout.fixed.horizontal,
+		set_status = function(self, image)
+			self.icon.image = image
+		end,
+		set_text = function(self, text)
+			self.current_song.markup = text
+		end,
+	}
+
+    self.dbus = proxy.monitored.new({
+        bus       = proxy.Bus.SESSION,
+        name      = "org.mpris.MediaPlayer2." .. args.name or "spotify",
+        path      = "/org/mpris/MediaPlayer2",
+        interface = "org.mpris.MediaPlayer2.Player"
+    })
+
+    -- Higher refresh_rate == less CPU requirements
+    -- Lower refresh_rate == better Widget response time
+	self:watch(args.refresh_rate or 3)
+	self:signal()
+
+	return self
+end
+
+function MediaPlayer:escape_xml(str)
+	str = string.gsub(str, "&", "&amp;")
+	str = string.gsub(str, "<", "&lt;")
+	str = string.gsub(str, ">", "&gt;")
+	str = string.gsub(str, "'", "&apos;")
+	str = string.gsub(str, '"', "&quot;")
+
+	return str
+end
+
+function MediaPlayer:update_widget_icon(output)
+	output = string.gsub(output, "\n", "")
+	self.widget:set_status(
+		(output == 'Playing') and self.icons.play or self.icons.pause
+	)
+end
+
+function MediaPlayer:update_widget_text(output)
+    self.widget:set_text(self:escape_xml(output))
+    self.widget:set_visible(true)
+end
+
+function MediaPlayer:hide_widget()
+    self.widget:set_text('Media Player | Offline')
+    self.widget:set_visible(false)
+end
+
+function MediaPlayer:info()
+  if not self.dbus.is_connected then
+    return {}
+  end
+
+  local metadata = self.dbus:Get(self.dbus.interface, "Metadata")
+  local status   = self.dbus:Get(self.dbus.interface, "PlaybackStatus")
+
+  local artists = metadata["xesam:artist"]
+  if type(artists) == "table" then
+    artists = table.concat(artists, ", ")
+  end
+
+  local info = {
+    album = metadata["xesam:album"],
+    title = metadata["xesam:title"],
+	artists = artists,
+	status = status
+  }
+
+  return info
+end
+
+function MediaPlayer:watch(refresh_rate)
+	local update_widget = function()
+		local info = self:info()
+        if not info["status"] then
+            self:hide_widget()
+        else
+            self:update_widget_icon(info["status"])
+            self:update_widget_text(
+                string.format(
+                    "%s | %s",
+                    info.artists,
+                    info.title
+                )
+            )
+        end
+	end
+
+	helpers.newtimer("media-player", refresh_rate, update_widget)
+end
+
+function MediaPlayer:exec(command, callback)
+	local spawn_update = function()
+		awful.spawn.easy_async(
+			function() self:info() end,
+			function(info)
+				self:update_widget_icon(info["status"])
+		end)
+	end
+
+	awful.spawn.easy_async(command, callback or spawn_update)
+end
+
+function MediaPlayer:signal()
+	--- Adds the following mouse actions:
+	--  - button 1: left click  - play/pause
+	--  - button 4: scroll up   - next song
+	--  - button 5: scroll down - previous song
+	
+	self.widget:buttons(awful.util.table.join(
+		awful.button({}, 1, function() self:exec("playerctl play-pause", _) end),
+		awful.button({}, 4, function() self:exec("playerctl next", _) end),
+		awful.button({}, 5, function() self:exec("playerctl previous", _) end)
+	))
+end
+
+return setmetatable(MediaPlayer, { __call = MediaPlayer.new, })
